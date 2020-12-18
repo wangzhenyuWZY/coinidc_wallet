be8f8b8add1b0448cd963ffd9ad6173fa70f096d
wangye <553311122@qq.com>
2020-12-18 18:22:04 +0800

    创建项目

--- src/router/router.config.js
+++ src/router/router.config.js
@@ -5,25 +5,11 @@
 export const constantRouterMap = [
   {
     path: '/',
-    component: () => import('@/views/layouts/index'),
-    redirect: '/home',
+    redirect:'/home',
     meta: {
       title: '首页',
       keepAlive: false
-    },
-    children: [
-      {
-        path: '/home',
-        name: 'Home',
-        component: () => import('@/views/home/index'),
-        meta: { title: '首页', keepAlive: false }
-      },
-      {
-        path: '/about',
-        name: 'About',
-        component: () => import('@/views/home/about'),
-        meta: { title: '关于我', keepAlive: false }
-      }
-    ]
-  }
+    }
+  },
+  { path: '/home', name: 'home', component: () => import('@/views/home/index')},
 ]
