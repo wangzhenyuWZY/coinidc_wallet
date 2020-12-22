9abef618a8fff6c8ebfaf50950500971810a7820
zhangyang <1105626801@qq.com>
2020-12-22 11:59:33 +0800

    安全区适配

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="app">
+    <!-- 开启顶部安全区适配 -->
+    <van-nav-bar safe-area-inset-top />
     <router-view />
+    <!-- 开启底部安全区适配 -->
+    <van-number-keyboard safe-area-inset-bottom />
   </div>
 </template>
 <script>
@@ -8,4 +12,3 @@ export default {
   name: 'App'
 }
 </script>
-<style lang="scss"></style>
