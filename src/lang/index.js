010c5bd3e2c4328c5147d033ed194dfae16a1cf6
wangye <553311122@qq.com>
2021-01-06 22:16:07 +0800

    1

--- src/lang/index.js
+++ src/lang/index.js
@@ -7,7 +7,7 @@ import zh from './config/zh'
 import en from './config/en'
 
 const i18n = new VueI18n({
-  locale: localStorage.getItem('lang') || 'en',
+  locale: localStorage.getItem('lang') || 'zh_CN',
   messages: {
     zh,
     en
